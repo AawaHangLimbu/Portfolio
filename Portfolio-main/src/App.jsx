3ab1f70e2224cf8d3570c597372d4aa023c1c03d
Aawa Hang Limbu <aawahang355@gmail.com>
2026-06-30 21:18:58 +0545

    fas

--- Portfolio-main/src/App.jsx
+++ Portfolio-main/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import StarWarp from './components/StarWarp';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -7,18 +8,20 @@ import TechStack from './components/TechStack';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-2
+
 function App() {
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
+
     if (element) {
       const headerOffset = 100;
       const elementPosition = element.getBoundingClientRect().top;
-      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-  
+      const offsetPosition =
+        elementPosition + window.pageYOffset - headerOffset;
+
       window.scrollTo({
         top: offsetPosition,
-        behavior: "smooth"
+        behavior: 'smooth',
       });
     }
   };
@@ -26,15 +29,16 @@ function App() {
   return (
     <div className="app-container">
       <StarWarp />
+
       <Navbar scrollToSection={scrollToSection} />
-      
+
       <main className="main-container">
         <Hero scrollToSection={scrollToSection} />
         <TechStack />
         <Projects />
         <Contact />
       </main>
-      
+
       <Footer />
     </div>
   );
